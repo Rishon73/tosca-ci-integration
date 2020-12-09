5a70edaecfb773e0a7b9385e4b203192f4399467
Rishon73 <s.shiff@tricentis.com>
2020-12-09 15:46:50 -0500

    run test is in ps1 script file

---
+++ run-tosca-test.ps1
@@ -0,0 +1,23 @@
+param ($resultsFile, $executionListFile)
+
+# Create the Results folder if it's not there
+if ( -not (Test-Path -LiteralPath 'Results' -PathType Container) ) { mkdir Results }
+
+# Get the 'COMMANDER_HOME' environment parameter
+$ToscaCommanderHome = [System.Environment]::GetEnvironmentVariable('COMMANDER_HOME')
+
+# This is how the command should look like
+#'...\ToscaCI\Client\ToscaCIClient.exe' -m distributed -t junit -x True -r 'results.xml' -c TestsConfiguration\api-execution-list.xml -e http://toscademovm:90/DistributionServerService/ManagerService.svc
+
+$ToscaCIClientExe = $ToscaCommanderHome + '\ToscaCI\Client\ToscaCIClient.exe'
+
+$m = 'distributed'
+$t = 'junit'
+$x = 'True'
+$r = $resultsFile
+$c = $executionListFile
+$e = 'http://toscademovm:90/DistributionServerService/ManagerService.svc'
+
+Write-Host "Running command: `n& $ToscaCIClientExe -m $m -t $t -x $x -r $r -c $c -e $e`n"
+
+& $ToscaCIClientExe -m $m -t $t -x $x -r $r -c $c -e $e
