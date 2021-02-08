47423a10a73f5a8cb7b17c7b8de7b81fa20b88c0
Rishon73 <s.shiff@tricentis.com>
2021-02-08 18:01:25 -0500

    removed commented-out code

--- publish-results-to-qtest.ps1
+++ publish-results-to-qtest.ps1
@@ -1,26 +1,21 @@
-param ($projectId, $testCycle, $uploadResultsFile)
+#param ($projectId, $testCycle, $uploadResultsFile)
 
-### We can't download Artifacts now, since the x509: certificate signed by unknown authority
-#if ($env:CI_PROJECT_DIR) {$content = (Get-Content '@Results/results.xml' -Raw)}
-#else  {$content = (Get-Content "Results/results.xml" -Raw)}
+$uploadResultsFile = "C:\ado-agent\_work\2\s\results.xml"
+$testCycle = "2193247"
+$projectId = "105466"
 
 Write-Host  "Debug -- Results file path: " $uploadResultsFile
 
-#$url = 'https://pulse-7.qtestnet.com/webhook/c3fe0169-a106-42e8-b7d9-06ee0cd924a1'
 $url = 'https://pulse-7.qtestnet.com/webhook/74595985-7445-4534-af21-b40bded3cb5f'
 $content = (Get-Content $uploadResultsFile -Raw)
-Write-Host  "Debug -- content: " $content
 $bytes = [System.Text.Encoding]::ASCII.GetBytes($content)
-Write-Host  "Debug -- bytes: " $bytes
 $payload = [System.Convert]::ToBase64String($bytes)
-Write-Host  "Debug -- payload: " $payload
 
 $body = @{
 	'projectId' = $projectId
 	'testcycle' = $testCycle
 	'result' = $payload
 }
-Write-Host  "Debug -- body: " $body
 
-#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#Invoke-RestMethod -Body $body -Method 'Post' -Uri $url
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod -Body $body -Method 'Post' -Uri $url
