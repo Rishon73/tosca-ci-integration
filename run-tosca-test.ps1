c361cdce84817fa5255e15021083c6dc8960ee58
Rishon73 <sshiff@gmail.com>
2021-03-18 18:26:03 -0400

    Some day Tosca server will be hosted and not local

--- run-tosca-test.ps1
+++ run-tosca-test.ps1
@@ -1,4 +1,4 @@
-param ($resultsFile, $executionListFile)
+param ($resultsFile='results.xml', $executionListFile='conf-execution-list.xml', $local='True')
 
 # Create the Results folder if it's not there
 if ( -not (Test-Path -LiteralPath 'Results' -PathType Container) ) { mkdir Results }
@@ -7,6 +7,9 @@ if ( -not (Test-Path -LiteralPath 'Results' -PathType Container) ) { mkdir Resul
 $ToscaCommanderHome = [System.Environment]::GetEnvironmentVariable('COMMANDER_HOME')
 $ToscaHostedServerAddrs = 'http://13.68.251.236:90'
 $ToscaLocalServerAddrs = 'http://toscademovm:90'
+
+if($local -eq 'True'){$ToscaServer = $ToscaLocalServerAddrs}
+else {$ToscaServer = $ToscaHostedServerAddrs}
 
 # This is how the command should look like
 #'...\ToscaCI\Client\ToscaCIClient.exe' -m distributed -t junit -x True -r 'results.xml' -c TestsConfiguration\api-execution-list.xml -e http://toscademovm:90/DistributionServerService/ManagerService.svc
@@ -18,7 +21,7 @@ $t = 'junit'
 $x = 'True'
 $r = $resultsFile
 $c = $executionListFile
-$e = $ToscaHostedServerAddrs+'/DistributionServerService/ManagerService.svc'
+$e = $ToscaServer+'/DistributionServerService/ManagerService.svc'
 
 Write-Host "Running command: `n& $ToscaCIClientExe -m $m -t $t -x $x -r $r -c $c -e $e`n"
 
