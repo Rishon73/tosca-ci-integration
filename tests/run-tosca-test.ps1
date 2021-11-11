07ff8cbc814cb07a9ea9fcd7e2d73cedfb6151b6
shahar shiff <sshiff@gmail.com>
2021-11-10 22:17:27 -0500

    toscaServer - new parameter. and deletin the remote tosca server part

--- tests/run-tosca-test.ps1
+++ tests/run-tosca-test.ps1
@@ -1,15 +1,10 @@
-param ($resultsFile='results\results.xml', $executionListFile='conf-execution-list.xml', $local='True')
+param ($resultsFile='results\results.xml', $executionListFile='conf-execution-list.xml', $toscaServer='http://toscademovm:5007')
 
 # Create the Results folder if it's not there
 if ( -not (Test-Path -LiteralPath 'Results' -PathType Container) ) { mkdir Results }
 
 # Get the 'COMMANDER_HOME' environment parameter
 $ToscaCommanderHome = [System.Environment]::GetEnvironmentVariable('COMMANDER_HOME')
-$ToscaHostedServerAddrs = 'http://13.68.251.236:90'
-$ToscaLocalServerAddrs = 'http://toscademovm:90'
-
-if($local -eq 'True'){$ToscaServer = $ToscaLocalServerAddrs}
-else {$ToscaServer = $ToscaHostedServerAddrs}
 
 # This is how the command should look like
 #'...\ToscaCI\Client\ToscaCIClient.exe' -m distributed -t junit -x True -r 'results.xml' -c TestsConfiguration\api-execution-list.xml -e http://toscademovm:90/DistributionServerService/ManagerService.svc
