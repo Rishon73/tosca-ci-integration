a6e1786da555bdfccbe2af40b4e883d1b7fd67d8
shahar shiff <sshiff@gmail.com>
2022-06-24 18:31:32 -0400

    add an example run command

---
+++ run test command example.ps1
@@ -0,0 +1,1 @@
+.\tests\aos-api\tosca_execution_client.ps1 -toscaServerUrl "http://localhost" -eventsConfigFilePath "tests\testEvents.json" -projectName "NEW_DEX_WS" -creator "Shahar Shiff" -debug "true"
