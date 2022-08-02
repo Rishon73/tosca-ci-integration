bd81c9a0ab780b6d88e19d9d45c0d90e3ec42477
shahar shiff <sshiff@gmail.com>
2022-08-02 13:56:22 -0400

    minor change

--- run test command example.ps1
+++ run test command example.ps1
@@ -5,9 +5,27 @@ Enqueue, poll for status and get results:
 
 Enqueue by event name/id, poll for status and get results:
   -----------------------------------------------------------
-  .\tests\aos-api\tosca_execution_client.ps1 -toscaServerUrl "http://localhost" -events '[{"eventId": "GetSetPerson", "eventId": "ado-pipeline"}]' -projectName "NEW_DEX_WS" -creator "Shahar Shiff" -debug  -pollingInterval 30 
+  .\tests\aos-api\tosca_execution_client.ps1 -toscaServerUrl "http://localhost" -events '[{"eventId": "GetSetPerson", "eventId": "ado-pipeline"}]' -projectName "NEW_DEX_WS" -creator "Shahar Shiff" -debug  -pollingInterval 30
 
 
 Enqueue only:
 ------------------------
-  .\tests\aos-api\tosca_execution_client.ps1 -toscaServerUrl "http://localhost" -eventsConfigFilePath "tests\testEvents.json" -projectName "NEW_DEX_WS" -creator "Shahar Shiff" -debug -enqueueOnly true
+.\tests\aos-api\tosca_execution_client.ps1 -toscaServerUrl "http://localhost" -eventsConfigFilePath "tests\testEvents.json" -projectName "NEW_DEX_WS" -creator "Shahar Shiff" -debug -enqueueOnly true
+
+
+"\tests\aos-api\tosca_execution_client.ps1" -toscaServerUrl "http://localhost" -eventsConfigFilePath "tests\testEvents.json" -projectName "NEW_DEX_WS" -creator "Shahar Shiff @jenkins" -debug "true"
+
+& "".\tosca_execution_client.ps1" -toscaServerUrl "http://localhost" -events '[{"eventId": "GetSetPerson"}]' -projectName "NEW_DEX_WS" -creator "ss from host" -debug "true"
+
+
+{
+  "ProjectName":"SQL Server Shared Workspace",
+  "ExecutionEnvironment":"true",
+  "Events":[
+    {
+      "eventId": "Renault TestEvent"
+    }
+  ],
+  "ImportResult":true,
+  "Creator":"Matt"
+}
