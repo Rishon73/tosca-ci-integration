2270f06347e0c102b096db117d57a359d41fe305
shahar shiff <sshiff@gmail.com>
2022-08-02 13:12:35 -0400

    more examples

--- run test command example.ps1
+++ run test command example.ps1
@@ -1,1 +1,13 @@
-.\tests\aos-api\tosca_execution_client.ps1 -toscaServerUrl "http://localhost" -eventsConfigFilePath "tests\testEvents.json" -projectName "NEW_DEX_WS" -creator "Shahar Shiff" -debug "true"
+Enqueue, poll for status and get results:
+------------------------------------------------
+  .\tests\aos-api\tosca_execution_client.ps1 -toscaServerUrl "http://localhost" -eventsConfigFilePath "tests\testEvents.json" -projectName "NEW_DEX_WS" -creator "Shahar Shiff" -debug
+
+
+Enqueue by event name/id, poll for status and get results:
+  -----------------------------------------------------------
+  .\tests\aos-api\tosca_execution_client.ps1 -toscaServerUrl "http://localhost" -events '[{"eventId": "GetSetPerson", "eventId": "ado-pipeline"}]' -projectName "NEW_DEX_WS" -creator "Shahar Shiff" -debug  -pollingInterval 30 
+
+
+Enqueue only:
+------------------------
+  .\tests\aos-api\tosca_execution_client.ps1 -toscaServerUrl "http://localhost" -eventsConfigFilePath "tests\testEvents.json" -projectName "NEW_DEX_WS" -creator "Shahar Shiff" -debug -enqueueOnly true
